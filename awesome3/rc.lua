1249926680387267a22b9c0f4b13302a67cfc45f
Julien Miotte <mike.perdide@gmail.com>
2010-12-16 20:12:25 +0100

    Forcing the display when launching kmail and korganizer.

--- awesome3/rc.lua
+++ awesome3/rc.lua
@@ -324,6 +324,10 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
+    { rule = { class = "kmail" },
+      properties = { tag = tags[1][9] } },
+    { rule = { class = "korganizer" },
+      properties = { tag = tags[1][9] } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
