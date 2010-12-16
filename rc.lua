09de1d1cae9b2d6dbdd67d8899f8e20a8c8a93f4
Mike Perdide <mike.perdide@gmail.com>
2010-12-16 09:30:46 +0100

    Defining custom theme and changing the name of the conf file.

--- rc.lua
+++ rc.lua
@@ -12,7 +12,8 @@ require("debian.menu")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+--beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("~/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
