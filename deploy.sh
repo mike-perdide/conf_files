2861714e22380c8fa142cad058e8a7a27f6c27f9
Julien Miotte <mike.perdide@gmail.com>
2010-12-16 09:24:53 +0100

    Better deployment, checking installation
    We check if the program is installed before deploying the scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,41 @@
 SCRIPTS_DIR=$PWD
 
-rm ~/.zshrc
-ln -s $SCRIPTS_DIR/zshrc ~/.zshrc
+check_installed(){
+    count=$(dpkg -l|grep " $1 "|wc -l) 
+    if (( $count != 0 ))
+    then
+        echo "yes"
+    fi
+}
 
-rm -r ~/.vim
-ln -s $SCRIPTS_DIR/vim ~/.vim
+deploy(){
+    # $1 is the program name (e.g. vim)
+    # $2 is the destination of the script (e.g. ~/.vimrc)
+    # $3 is the source of the script (e.g. ~/conffiles/vimrc)
+    is_installed=$(check_installed $1)
+    if [ $is_installed ]
+    then
+        rm -r $2
+        ln -s $3 $2
+    fi
+}
 
-rm ~/.vimrc
-ln -s $SCRIPTS_DIR/vimrc ~/.vimrc
 
-if [ ! -L ~/.gitconfig ]
-then
-    rm ~/.gitconfig
-    ln -s $SCRIPTS_DIR/gitconfig_home ~/.gitconfig
-fi
+deploy zsh ~/.zshrc $SCRIPTS_DIR/zshrc
 
-rm ~/.lynxrc
-ln -s $SCRIPTS_DIR/lynxrc ~/.lynxrc
+deploy vim ~/.vim $SCRIPTS_DIR/vim
+deploy vim ~/.vimrc $SCRIPTS_DIR/vimrc
 
-rm -r ~/.newsbeuter
-ln -s $SCRIPTS_DIR/newsbeuter ~/.newsbeuter
+deploy git-core ~/.gitconfig ln -s $SCRIPTS_DIR/gitconfig_home
 
-rm -r ~/.screenrc
-ln -s $SCRIPTS_DIR/screenrc ~/.screenrc
+deploy lynx ~/.lynxrc $SCRIPTS_DIR/lynxrc
 
-rm ~/.xmodmap
-ln -s $SCRIPTS_DIR/xmodmap ~/.xmodmap
+deploy newsbeuter ~/.newsbeuter $SCRIPTS_DIR/newsbeuter
 
-rm -r ~/.irssi
-ln -s $SCRIPTS_DIR/irssi ~/.irssi
+deploy screen ~/.screenrc $SCRIPTS_DIR/screenrc
+
+deploy bash ~/.xmodmap $SCRIPTS_DIR/xmodmap
+
+deploy irssi ~/.irssi $SCRIPTS_DIR/irssi
+
+deploy xserver-xorg-core ~/.Xdefaults $SCRIPTS_DIR/Xdefaults
