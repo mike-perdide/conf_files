0636334dff5be5c3abd654861454718afe7a6d21
Julien MIOTTE <j.m@majerti.fr>
2021-10-16 20:48:29 +0200

    Update rc.lua

--- awesome3/rc.lua
+++ awesome3/rc.lua
@@ -53,10 +53,10 @@ layouts =
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
 tags = {}
-for s = 1, screen.count() do
+--for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
-end
+tags[0] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+--end
 -- }}}
 
 -- {{{ Menu
